1e2c39b25289609d3c6ab012bbfceb86061be6f4
nishant <nishant.singh87@gmail.com>
2015-05-28 02:48:20 +0530

    removing unused code.

--- src/main/java/chakra/web/Runner.java
+++ src/main/java/chakra/web/Runner.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-
 @RestController
 @RequestMapping("/runner")
 public class Runner {
