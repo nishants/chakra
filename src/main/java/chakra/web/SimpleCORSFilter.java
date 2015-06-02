2be784a1476363f9de4275f3e498346b9875b820
nishant <nishant.singh87@gmail.com>
2015-06-02 12:14:16 +0530

    adding cross domain ajax calls

--- src/main/java/chakra/web/SimpleCORSFilter.java
+++ src/main/java/chakra/web/SimpleCORSFilter.java
@@ -10,8 +10,17 @@ public class SimpleCORSFilter implements Filter {
 
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) res;
+
     response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+
+    response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+    response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    response.setHeader("Access-Control-Max-Age", "3600");
+
     chain.doFilter(req, res);
+    System.out.println("cross origin working.........");
   }
 
   public void init(FilterConfig filterConfig) {}
